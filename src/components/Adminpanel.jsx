820e16ab5fb4b26f1ac8480c71e512bac8053a4a
MaxiBlanc <maxiblanc240801@gmail.com>
2026-02-18 20:44:15 -0300

    clave

--- src/components/Adminpanel.jsx
+++ src/components/Adminpanel.jsx
@@ -5,7 +5,7 @@ import {
   doc, deleteDoc, updateDoc, writeBatch, getDocs, where 
 } from 'firebase/firestore';
 import Swal from 'sweetalert2';
-import './AdminPanel.css';
+import './Adminpanel.css';
 
 const AdminPanel = () => {
     const [categorias, setCategorias] = useState([]);
